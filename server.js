ae1bcc584a26088b362751850684fd22c5fa510e
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2020-04-07 16:53:50 +0900

    在庫数量の変更を行うエンドポイント定義

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Simple REST API');
+});
+
+app.put('/api/v1/inventories/:store/:jan', (req, res) => {
+  const jan = req.params.jan
+  if (!jan) return res.status(404).send('The given JAN was not found.');
+
+  res.send({
+    store: req.params.store,
+    jan: req.params.jan,
+    qty: req.body.qty
+  });
 });
 
 const port = process.env.PORT || 3000;
